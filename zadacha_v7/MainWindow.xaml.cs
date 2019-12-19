b5e552e5e7d7cd2b6117f5991325e702dd9778ef
Данил Долотовский <atmo132@outlook.com>
2019-12-20 01:09:43 +0300

    Код без оптимизации.

---
+++ zadacha_v7/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zadacha_v7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetAnswerClick(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(txtbxForA.Text);
+            double b = Convert.ToDouble(txtbxForB.Text);
+            double h = Convert.ToDouble(txtbxForH.Text);
+
+            double x = a;
+            double y;
+
+
+
+            while (x <= b)
+            {
+                y = x - Math.Sin(x);
+                string answer = $"\t{x}\t\t{y}\n";
+                txtbxAnswer.Text += answer;
+                x = x + h;
+            }
+        }
+    }
+}
